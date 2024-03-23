226b9bddd31e32759c67f8af6676ee0f0c035ac8
Carmenchu <carmenxfdz@gmail.com>
2024-03-23 17:05:18 +0100

    EJ4

--- practica1/Ejercicio4/src/ejercicio4/CalcularVelocidad.java
+++ practica1/Ejercicio4/src/ejercicio4/CalcularVelocidad.java
@@ -9,5 +9,38 @@ package ejercicio4;
  * @author carme
  */
 public class CalcularVelocidad implements Filtro{
-    
+    private double incrementoVelocidad;
+    private int MAX_REVOLUCIONES = 5000;
+
+    CalcularVelocidad(){
+        this.incrementoVelocidad = 0;
+    }
+
+    public double ejecutar(double revoluciones, EstadoMotor estadomotor){
+        switch (estadomotor) {
+            case APAGADO:
+                incrementoVelocidad = 0;
+                break;
+            
+            case ENCENDIDO:
+                incrementoVelocidad = 0;
+                break;
+        
+            case ACELERANDO:
+                incrementoVelocidad = 100;
+                break;
+
+            case FRENANDO:
+                incrementoVelocidad = -100;
+                break;
+
+            default:
+                break;
+        }
+
+        if((revoluciones + incrementoVelocidad) <= MAX_REVOLUCIONES)
+            revoluciones += incrementoVelocidad;
+
+        return revoluciones;
+    }
 }
--- practica1/Ejercicio4/src/ejercicio4/RepercutirRozamiento.java
+++ practica1/Ejercicio4/src/ejercicio4/RepercutirRozamiento.java
@@ -9,5 +9,13 @@ package ejercicio4;
  * @author carme
  */
 public class RepercutirRozamiento implements Filtro{
-    
+    private int ROZAMIENTO;
+
+    RepercutirRozamiento(){
+        this.ROZAMIENTO = 30;
+    }
+
+    public double ejecutar(double revoluciones, EstadoMotor estadomotor){
+        return revoluciones - ROZAMIENTO;
+    }
 }
