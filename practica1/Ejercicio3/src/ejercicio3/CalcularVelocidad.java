57664939e6572fcefffc5be5ec85dca97cd8928a
pablogarciagui <127139692+pablogarciagui@users.noreply.github.com>
2024-03-23 16:53:04 +0100

    ejecutar del calcularvelocidad

--- practica1/Ejercicio3/src/ejercicio3/CalcularVelocidad.java
+++ practica1/Ejercicio3/src/ejercicio3/CalcularVelocidad.java
@@ -1,7 +1,34 @@
 public class CalcularVelocidad implements Filtro{
     private double incrementoVelocidad;
+    private int MAX_REVOLUCIONES = 5000;
 
     CalcularVelocidad(){
         this.incrementoVelocidad = 0;
+    }
+
+    double ejecutar(double revoluciones, EstadoMotor estadomotor){
+        switch (estadomotor) {
+            case APAGADO:
+                incrementoVelocidad = 0;
+                break;
+            
+            case ENCENDIDO:
+                incrementoVelocidad = 0;
+                break;
+        
+            case ACELERANDO:
+                incrementoVelocidad = 100;
+                break;
+
+            case FRENANDO:
+                incrementoVelocidad = -100;
+                break;
+
+            default:
+                break;
+        }
+
+        if((revoluciones + incrementoVelocidad) <= MAX_REVOLUCIONES)
+            revoluciones += incrementoVelocidad;
     }
 }
