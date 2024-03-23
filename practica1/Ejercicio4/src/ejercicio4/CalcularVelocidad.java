f2169c2d728b4e4f7928c51d710831c0540b092e
Carmenchu <carmenxfdz@gmail.com>
2024-03-23 17:12:07 +0100

    Arreglo Filtros

--- practica1/Ejercicio4/src/ejercicio4/CalcularVelocidad.java
+++ practica1/Ejercicio4/src/ejercicio4/CalcularVelocidad.java
@@ -1,3 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio4;
+
+
+import controlador.EstadoMotor;
+
 public class CalcularVelocidad implements Filtro{
     private double incrementoVelocidad;
 
@@ -9,6 +18,7 @@ public class CalcularVelocidad implements Filtro{
     }
 
 
+    @Override
     public double ejecutar(double revoluciones, EstadoMotor estadomotor){
         switch (estadomotor) {
             case APAGADO:
--- practica1/Ejercicio4/src/ejercicio4/RepercutirRozamiento.java
+++ practica1/Ejercicio4/src/ejercicio4/RepercutirRozamiento.java
@@ -1,3 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio4;
+
+import controlador.EstadoMotor;
+
 public class RepercutirRozamiento implements Filtro{
     private int ROZAMIENTO;
 
@@ -6,10 +14,8 @@ public class RepercutirRozamiento implements Filtro{
         this.ROZAMIENTO = 30;
     }
 
+    @Override
     public double ejecutar(double revoluciones, EstadoMotor estadomotor){
         return revoluciones - ROZAMIENTO;
-
-        this.ROZAMIENTO = 0;
-
     }
 }
