d1d3b8b39e009aedfd4263c9f912f94bd2cc3042
pablogarciagui <127139692+pablogarciagui@users.noreply.github.com>
2024-03-23 17:05:18 +0100

    arreglo

--- practica1/Ejercicio4/src/ejercicio4/CalcularVelocidad.java
+++ practica1/Ejercicio4/src/ejercicio4/CalcularVelocidad.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ejercicio4;
-
-/**
- *
- * @author carme
- */
 public class CalcularVelocidad implements Filtro{
-    
+    private double incrementoVelocidad;
+    private int MAX_REVOLUCIONES = 5000;
+
+    CalcularVelocidad(){
+        this.incrementoVelocidad = 0;
+    }
+
+    public double ejecutar(double revoluciones, EstadoMotor estadomotor){
+        switch (estadomotor) {
+            case APAGADO:
+                incrementoVelocidad = 0;
+                break;
+            
+            case ENCENDIDO:
+                incrementoVelocidad = 0;
+                break;
+        
+            case ACELERANDO:
+                incrementoVelocidad = 100;
+                break;
+
+            case FRENANDO:
+                incrementoVelocidad = -100;
+                break;
+
+            default:
+                break;
+        }
+
+        if((revoluciones + incrementoVelocidad) <= MAX_REVOLUCIONES)
+            revoluciones += incrementoVelocidad;
+
+        return revoluciones;
+    }
 }
--- practica1/Ejercicio4/src/ejercicio4/RepercutirRozamiento.java
+++ practica1/Ejercicio4/src/ejercicio4/RepercutirRozamiento.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ejercicio4;
-
-/**
- *
- * @author carme
- */
 public class RepercutirRozamiento implements Filtro{
-    
+    private int ROZAMIENTO;
+
+    RepercutirRozamiento(){
+        this.ROZAMIENTO = 30;
+    }
+
+    public double ejecutar(double revoluciones, EstadoMotor estadomotor){
+        return revoluciones - ROZAMIENTO;
+    }
 }
